3a6c2729d804c7539e8ab3b28c66465681b0a1e2
mhcodner <mhcodner@gmail.com>
2015-03-11 15:58:39 +1100

    Succesfully convert decimal to binary

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(function() {
 	$("#assemble").click(function(){
-		$("#machine").val() = $("#assembly").val();
+		decimal = $("#assembly").val();
+		binary = dec2Bin(decimal);
+		$("#machine").val(binary);
 	});
 });
+
+function dec2Bin(dec)
+{
+    return (dec >>> 0).toString(2);
+}
