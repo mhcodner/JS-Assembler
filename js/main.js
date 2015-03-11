140edd270c0a5b7149848d710df61063f6998d81
mhcodner <mhcodner@gmail.com>
2015-03-11 16:18:38 +1100

    Padded to 6 bits

--- js/main.js
+++ js/main.js
@@ -8,5 +8,9 @@ $(function() {
 
 function dec2Bin(dec)
 {
-    return (dec >>> 0).toString(2);
+	binstr = (dec >>> 0).toString(2);
+	while(binstr.length < 6) {
+		binstr = "0" + binstr;
+	}
+    return binstr;
 }
