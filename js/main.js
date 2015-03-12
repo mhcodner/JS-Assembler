f71bb0097487159317a664e1882114ebc39df0dc
mhcodner <mhcodner@users.noreply.github.com>
2015-03-12 17:02:19 +1100

    Improved error handling
    Highlight the line with error and display better error messages

--- js/main.js
+++ js/main.js
@@ -10,24 +10,31 @@ $(function() {
 			opCode = getOpCode(code[0]); // attempt to get the corresponding opcode for the first 'word'
 			if (opCode === "error")
 			{
-				alert("Invalid operation")
+				tarea = document.getElementById('assembly');
+				selectTextareaLine(tarea, i + 1);
+				alert("Invalid operation: " + code[0]);
+				return false;
 			}
 			if (opCode === "111") // the rest of the output is 0s if you get HALT opcode
 			{
 				numberBit = "0";
 				binary = "000000"
+				returnCode.push(opCode + " " + numberBit + " " + binary);
+				return false;
 			}
 			else
 			{
 				numberBit = getNumberBit(code[1]);
 				number = code[1];
+				maxOperand = 63
 				if (numberBit == 1) // get the operand
 				{
 					number = code[1].substring(1);
 				}
-				if (number > 63)
+				if (number > maxOperand)
 				{
-					alert("Operand out of bounds");
+					alert("Operand out of bounds, operand must be <= " + maxOperand);
+					return false;
 				}
 				binary = dec2Bin(number); // convert to the operand to binary
 			}
@@ -73,5 +80,45 @@ function dec2Bin(dec)
 	while(binstr.length < 6) { // pad with 0s to make it 6 bits
 		binstr = "0" + binstr;
 	}
-    return binstr;
+	return binstr;
+}
+
+function selectTextareaLine(tarea,lineNum) {
+	lineNum--; // array starts at 0
+	var lines = tarea.value.split("\n");
+
+	// calculate start/end
+	var startPos = 0, endPos = tarea.value.length;
+	for(var x = 0; x < lines.length; x++) {
+		if(x == lineNum) {
+			break;
+		}
+		startPos += (lines[x].length+1);
+	}
+
+	var endPos = lines[lineNum].length+startPos;
+
+	// do selection
+	// Chrome / Firefox
+
+	if(typeof(tarea.selectionStart) != "undefined") {
+		tarea.focus();
+		tarea.selectionStart = startPos;
+		tarea.selectionEnd = endPos;
+		return true;
+	}
+
+	// IE
+	if (document.selection && document.selection.createRange) {
+		tarea.focus();
+		tarea.select();
+		var range = document.selection.createRange();
+		range.collapse(true);
+		range.moveEnd("character", endPos);
+		range.moveStart("character", startPos);
+		range.select();
+		return true;
+	}
+
+	return false;
 }
