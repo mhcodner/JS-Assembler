deaf34397afbe9ce29d8be7b76ae01a85425a2c2
mhcodner <mhcodner@gmail.com>
2015-03-18 16:47:44 +1100

    Implemented exceptions

--- js/main.js
+++ js/main.js
@@ -1,46 +1,73 @@
+tarea = document.getElementById('assembly');
+
 function assemble(){
 	assembly = document.getElementById("assembly").value; // gets the value of the textarea for the assembly code
 	linesOfCode = assembly.split("\n"); // creates an array of each line of assembly
 	machineCode = ""; 
 	returnCode = [];
-	linesOfCode.forEach(function(line, i){ // loop over each line of code and get the index of the loop
-		re = new RegExp(/\s+/); // regex for one or more spaces
-		code = line.split(re); // split each line of code by the spaces to get each separate 'word'
-		opCode = getOpCode(code[0]); // attempt to get the corresponding opcode for the first 'word'
-		if (opCode === "error")
-		{
-			tarea = document.getElementById('assembly');
-			selectTextareaLine(tarea, i + 1);
-			alert("Invalid operation: " + code[0]);
-			return false;
-		}
-		if (opCode === "111") // the rest of the output is 0s if you get HALT opcode
-		{
-			numberBit = "0";
-			binary = "000000"
-			returnCode.push(opCode + " " + numberBit + " " + binary);
-			return false;
-		}
-		else
-		{
-			numberBit = getNumberBit(code[1]);
-			number = code[1];
-			maxOperand = 63
-			if (numberBit == 1) // get the operand
+	try
+	{
+		linesOfCode.forEach(function(line, i){ // loop over each line of code and get the index of the loop
+			re = new RegExp(/\s+/); // regex for one or more spaces
+			code = line.split(re); // split each line of code by the spaces to get each separate 'word'
+			opCode = getOpCode(code[0]); // attempt to get the corresponding opcode for the first 'word'
+			if (opCode === "error")
 			{
-				number = code[1].substring(1);
+				throw new InvalidOperationException(code[0], i);
 			}
-			if (number > maxOperand)
+			if (opCode === "111") // the rest of the output is 0s if you get HALT opcode
 			{
-				alert("Operand out of bounds, operand must be <= " + maxOperand);
-				return false;
+				numberBit = "0";
+				binary = "000000"
+				returnCode.push(opCode + " " + numberBit + " " + binary);
+				throw HaltException;
 			}
-			binary = dec2Bin(number); // convert to the operand to binary
+			else
+			{
+				numberBit = getNumberBit(code[1]);
+				number = code[1];
+				maxOperand = 63
+				if (numberBit == 1) // get the operand
+				{
+					number = code[1].substring(1);
+				}
+				if (number > maxOperand)
+				{
+					throw new OperandOutOfBoundsException(maxOperand, i);
+				}
+				binary = dec2Bin(number); // convert to the operand to binary
+			}
+			returnCode.push(opCode + " " + numberBit + " " + binary); // add the string for this line of code to the array
+		});
+	} catch (e)
+	{
+		if (e.name === "InvalidOperation")
+		{
+			selectTextareaLine(tarea, e.line);
+			alert(e.message);
 		}
-		returnCode.push(opCode + " " + numberBit + " " + binary); // add the string for this line of code to the array
-	});
+		if (e.name === "InvalidOperand")
+		{
+			selectTextareaLine(tarea, e.line);
+			alert(e.message);
+		}
+	}
 	machineCode = returnCode.join("\n"); // join the array into a single string separated by new lines
 	document.getElementById("machine").value = machineCode; // add the machine to the appropriate textarea
+}
+
+function InvalidOperationException(message, i)
+{
+	this.name = "InvalidOperation";
+	this.line = i + 1;
+	this.message = "Invalid operation: " + message;
+}
+
+function OperandOutOfBoundsException(maxOperand, i)
+{
+	this.name = "InvalidOperand";
+	this.line = i + 1;
+	this.message = "Operand out of bounds, operand must be <= " + maxOperand;
 }
 
 function getNumberBit(code)
